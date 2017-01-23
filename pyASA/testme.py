de4c2fc669914cfe49554c09ada1fbe9ebda0010
bria\xpac <bjoern@areafunky.net>
2017-01-23 22:59:34 +0100

    testme.py: removed from git tracking

--- pyASA/testme.py
+++ pyASA/testme.py
@@ -17,21 +17,38 @@ if asa.test_connection():
     #     rule.dst_port = port
     #     asa.acl.append_rule("ALIASES", rule)
 
-    # rules = []
-    # for port in range(99, 100):
-    #     rule = RuleTCPUDP()
-    #     rule.protocol = "udp"
-    #     rule.dst_port = port
-    #     rules.append(rule)
-    # # rules.append(rule)
-    # asa.acl.append_rules("ALIASES-UDP", rules)
+    # if asa.acl.exists("TESTTEST"):
+    #     rules = asa.acl.get_rules("TESTTEST")
+    #     objectids = [rule.objectid for  rule in rules]
+    #     for objectid in objectids:
+    #         asa.acl.delete_rule("TESTTEST", objectid)
 
-    if asa.acl.exists("ALIASES-UDP"):
-        rules = asa.acl.get_rules("ALIASES-UDP")
-        objectids = [rule.objectid for __, rule in rules.items()]
-        for objectid in objectids:
-            asa.acl.delete_rule("ALIASES-UDP", objectid)
+    if asa.acl.exists("TESTTEST"):
+        asa.acl.delete_rules("TESTTEST")
 
+    rules = []
+    for port in range(1, 11):
+        rule = RuleTCPUDP()
+        rule.protocol = "udp"
+        rule.dst_port = port + 33
+        rule.position = 0
+        rules.append(rule)
+    asa.acl.append_rules("TESTTEST", rules)
 
+    print(asa.acl.get_rules("TESTTEST"))
+
+    if asa.acl.exists("TESTTEST"):
+        rules = asa.acl.get_rules("TESTTEST")
+        objectids = [rule.objectid for rule in rules]
+        asa.acl.delete_rules("TESTTEST", objectids)
+
+    rules = []
+    for port in range(1, 11):
+        rule = RuleTCPUDP()
+        rule.protocol = "udp"
+        rule.dst_port = port + 66
+        rule.position = 0
+        rules.append(rule)
+    asa.acl.append_rules("TESTTEST", rules)
 
 print("END Test")
